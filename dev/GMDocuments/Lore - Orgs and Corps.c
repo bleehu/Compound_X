75b6011e8a6821c79eb5321204ab6265440f6c36
trowl223 <t.rowland223@gmail.com>
2016-09-26 20:51:10 -0700

    Tons of GM documentation including lore on alien races and lore on the histroy of humanity so far

---
+++ dev/GMDocuments/Lore - Orgs and Corps.c
@@ -0,0 +1,10 @@
+//====================================================
+// 
+//  On Organisations and Corporations
+//
+//
+//====================================================
+
+Coprorations control regions within the sectors, and are usually local to only those sectors. They are very large, but very few span multiple sectors
+
+Organsations are the entities responsible for pioneering space travel etc. and are concerned with finding artifacts. they differ from corporations in the sense that they have been araound since the onset of space travel, and are larger now then mere profit-based businesses. their goals vary from introuducing the tech they find to all people, to locking it up so no one can use it for its destrucive puroses, to simply destroying it. they are working against each other or with each other.
