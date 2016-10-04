a3e129eeaf2af57331db171248acc1bbe892e29d
bleehu <bleehu@uw.edu>
2016-10-03 17:49:09 -0700

    added human-readable constants to remove some magic numbers

--- dev/GMDocuments/Programs/randGun.c
+++ dev/GMDocuments/Programs/randGun.c
@@ -9,10 +9,30 @@
 #define LEVEL 10//specify either 0, 5, 10, 15 or 20 for appropriate
                 //scaling with enemy strength
 
+//Gun Types to be stored in stat[0]
+#define PISTOL 0
+#define SMG 1
+#define SHOTGUN 2
+#define CARBINE 3
+#define AUTOMATIC 4
+#define HEAVY 5
+#define LONG_RIFLE 6
 
+//stats in the stat array
+#define TYPE 0
+#define RANGE 1
+#define MISS 2
+#define CAPACITY 3
+#define DAMAGE 4
+#define FIRE_TYPE 5
+#define COST 6
+
+//Firetypes
+#define SINGLEFIRE 0
+#define BOLT_ACTION 1
 
  int main () {
-    printf("Running Weapon Generator Version A_1.0\n");
+    printf("Running Weapon Generator Version A_1.1\n");
 
     time_t t;
     srand((unsigned) time(&t));
@@ -30,21 +50,21 @@
     //Each weapon type should have an equal liklihood of being
     //generated, so we have 7 "type" outcomes. based on what "type"
     //it is, we generate different values for most of the stats.
-    stats[0] = rand()%7;
+    stats[TYPE] = rand()%7;
 
 
     //determine range, 1
-    if (stats[0] == 0) {//Pistol
-        stats[1] = (5 * (rand() % 6)) + 30 + (LEVEL*2); //30-55 base
-    } else if (stats[0] == 1) {//SMG
-        stats[1] = (5 * (rand() % 8)) + 45 + (LEVEL*2); //45-80 base
-    } else if (stats[0] == 2) {//Shotgun
-        stats[1] = 20 + LEVEL;//20 base
-    } else if (stats[0] == 3) {//Carbines/BRs
-        stats[1] = (5 * (rand() % 16)) + 75 + (LEVEL*4); //75-150 base
-    } else if (stats[0] == 4) {//Automatic Rifles
-        stats[1] = (5 * (rand() % 11)) + 80 + (LEVEL*4); //80-130 base
-    } else if (stats[0] == 5) {//Heavy Weapons
+    if (stats[TYPE] == PISTOL) {//Pistol
+        stats[RANGE] = (5 * (rand() % 6)) + 30 + (LEVEL*2); //30-55 base
+    } else if (stats[TYPE] == SMG) {//SMG
+        stats[RANGE] = (5 * (rand() % 8)) + 45 + (LEVEL*2); //45-80 base
+    } else if (stats[TYPE] == SHOTGUN) {//Shotgun
+        stats[RANGE] = 20 + LEVEL;//20 base
+    } else if (stats[TYPE] == CARBINE) {//Carbines/BRs
+        stats[RANGE] = (5 * (rand() % 16)) + 75 + (LEVEL*4); //75-150 base
+    } else if (stats[TYPE] == AUTOMATIC) {//Automatic Rifles
+        stats[RANGE] = (5 * (rand() % 11)) + 80 + (LEVEL*4); //80-130 base
+    } else if (stats[TYPE] == HEAVY) {//Heavy Weapons
         //50-200 base
         int raw = rand() % 12;
         if (raw > 5) {
@@ -54,58 +74,58 @@
             raw *= 5;
             raw += 50;
         }
-        stats[1] = raw + (3 * LEVEL);
-    } else if (stats[0] == 6) {//Long Rifles
-        stats[1] = (5 * (rand() % 7)) + 30 + (LEVEL*15); //150-500 base
+        stats[RANGE] = raw + (3 * LEVEL);
+    } else if (stats[TYPE] == LONG_RIFLE) {//Long Rifles
+        stats[RANGE] = (5 * (rand() % 7)) + 30 + (LEVEL*15); //150-500 base
     }
     
 
-    //determin percent miss, 2
-    stats[2] = (rand()%(30 - LEVEL));
+    //determine percent miss, 2
+    stats[MISS] = (rand()%(30 - LEVEL));
 
     //Determine fire type, 5; 0 single, 1 semi, 2 full
-    if (stats[0] == 0) {//Pistol
-        stats[5] = rand()%2;
-    } else if (stats[0] == 1) {//Smg
-        stats[5] = 2;
-    } else if (stats[0] == 2) { //Shotgun
-        stats[5] = rand()%2;
-    } else if (stats[0] == 3) {//Carbine
-        stats[5] = 1; 
-    } else if (stats[0] == 4) {//Assault Rifle
-        stats[5] = 2;
-    } else if (stats[0] == 5) {//Heavy Weapon
-        stats[5] = rand()%3;
-    } else if (stats[0] == 6) {//Long Rifle
-        stats[5] = rand()%2;
+    if (stats[TYPE] == PISTOL) {//Pistol
+        stats[FIRE_TYPE] = rand()%2;
+    } else if (stats[TYPE] == SMG) {//Smg
+        stats[FIRE_TYPE] = 2;
+    } else if (stats[TYPE] == SHOTGUN) { //Shotgun
+        stats[FIRE_TYPE] = rand()%2;
+    } else if (stats[TYPE] == CARBINE) {//Carbine
+        stats[FIRE_TYPE] = 1; 
+    } else if (stats[TYPE] == AUTOMATIC) {//Assault Rifle
+        stats[FIRE_TYPE] = 2;
+    } else if (stats[TYPE] == HEAVY) {//Heavy Weapon
+        stats[FIRE_TYPE] = rand()%3;
+    } else if (stats[TYPE] == LONG_RIFLE) {//Long Rifle
+        stats[FIRE_TYPE] = rand()%2;
     }
 
     //determine capacity, 3
-    if (stats[0] == 0) {//Pistol
-        if (stats[5]) {
-            stats[3] = rand()%10 + 7;
+    if (stats[TYPE] == PISTOL) {//Pistol
+        if (stats[FIRE_TYPE]) {
+            stats[CAPACITY] = rand()%10 + 7;
         } else {
-            stats[3] = rand()%3 + 5;
+            stats[CAPACITY] = rand()%3 + 5;
         }
-    } else if (stats[0] == 1) {//Smg
-        stats[3] = (5*rand()%3)+25;
-    } else if (stats[0] == 2) { //Shotgun
+    } else if (stats[TYPE] == SMG) {//Smg
+        stats[CAPACITY] = (5*rand()%3)+25;
+    } else if (stats[TYPE] == SHOTGUN) { //Shotgun
         int cap = rand()%3;
         if (cap == 0) {
-            stats[3] = 5;
+            stats[CAPACITY] = 5;
         } else if (cap == 1) {
-            stats[3] = 6;
+            stats[CAPACITY] = 6;
         } else {
-            stats[3] = 7;
+            stats[CAPACITY] = 7;
         }
-    } else if (stats[0] == 3) {//Carbine
-        stats[3] = (5*rand()%3)+15;
-    } else if (stats[0] == 4) {//Assault Rifle
-        stats[3] = (5*rand()%3)+25;
-    } else if (stats[0] == 5) {//Heavy Weapon
-        stats[3] = (5*rand()%3)+25;
-    } else if (stats[0] == 6) {//Long Rifle
-        stats[3] = (rand()%10)+5;
+    } else if (stats[TYPE] == CARBINE) {//Carbine
+        stats[CAPACITY] = (5*rand()%3)+15;
+    } else if (stats[TYPE] == AUTOMATIC) {//Assault Rifle
+        stats[CAPACITY] = (5*rand()%3)+25;
+    } else if (stats[TYPE] == HEAVY) {//Heavy Weapon
+        stats[CAPACITY] = (5*rand()%3)+25;
+    } else if (stats[TYPE] == LONG_RIFLE) {//Long Rifle
+        stats[CAPACITY] = (rand()%10)+5;
     }
 
     //Determine Damage, 4
@@ -114,92 +134,92 @@
     //62, 68, 70, 95, 115, 74, 85
 
     int damType;
-    if (stats[0] == 0 || stats[0] == 1) {//Pistol & SMG
+    if (stats[TYPE] == 0 || stats[TYPE] == 1) {//Pistol & SMG
         damType = rand()%10;
         switch (damType) {
             case 0:
-                stats[4] = 30;
+                stats[DAMAGE] = 30;
             break;
             case 1:
-                stats[4] = 35;
+                stats[DAMAGE] = 35;
             break;
             case 2:
-                stats[4] = 41;
+                stats[DAMAGE] = 41;
             break;
             case 3:
-                stats[4] = 43;
+                stats[DAMAGE] = 43;
             break;
             case 4:
-                stats[4] = 44;
+                stats[DAMAGE] = 44;
             break;
             case 5:
-                stats[4] = 46;
+                stats[DAMAGE] = 46;
             break;
             case 6:
-                stats[4] = 47;
+                stats[DAMAGE] = 47;
             break;
             case 7:
-                stats[4] = 52;
+                stats[DAMAGE] = 52;
             break;
             case 8:
-                stats[4] = 56;
+                stats[DAMAGE] = 56;
             break;
             case 9:
-                stats[4] = 60;
+                stats[DAMAGE] = 60;
             break;
         }
-    } else if (stats[0] == 2) {//Shotgun
-        stats[4] = 80;
-    } else if (stats[0] == 3 || stats[0] == 4) {//Carbine / BR & Automatic Rifles
+    } else if (stats[TYPE] == SHOTGUN) {//Shotgun
+        stats[DAMAGE] = 80;
+    } else if (stats[TYPE] == CARBINE || stats[TYPE] == AUTOMATIC) {//Carbine / BR & Automatic Rifles
         damType = rand()%7;
         switch (damType) {
             case 0:
-                stats[4] = 60;
+                stats[DAMAGE] = 60;
             break;
             case 1:
-                stats[4] = 75;
+                stats[DAMAGE] = 75;
             break;
             case 2:
-                stats[4] = 90;
+                stats[DAMAGE] = 90;
             break;
             case 3:
-                stats[4] = 50;
+                stats[DAMAGE] = 50;
             break;
             case 4:
-                stats[4] = 55;
+                stats[DAMAGE] = 55;
             break;
             case 5:
-                stats[4] = 56;
+                stats[DAMAGE] = 56;
             break;
             case 6:
-                stats[4] = 57;
+                stats[DAMAGE] = 57;
             break;
         }
-    } else if (stats[0] == 5) {//Heavy Weapon
-        stats[0] = 0;
-    } else if (stats[0] == 6) {//Long Rifle
+    } else if (stats[TYPE] == HEAVY) {//Heavy Weapon
+        stats[TYPE] = 0;
+    } else if (stats[TYPE] == LONG_RIFLE) {//Long Rifle
         damType = rand()%7;
         switch (damType) {
             case 0:
-                stats[4] = 62;
+                stats[DAMAGE] = 62;
             break;
             case 1:
-                stats[4] = 68;
+                stats[DAMAGE] = 68;
             break;
             case 2:
-                stats[4] = 70;
+                stats[DAMAGE] = 70;
             break;
             case 3:
-                stats[4] = 95;
+                stats[DAMAGE] = 95;
             break;
             case 4:
-                stats[4] = 115;
+                stats[DAMAGE] = 115;
             break;
             case 5:
-                stats[4] = 74;
+                stats[DAMAGE] = 74;
             break;
             case 6:
-                stats[4] = 85;
+                stats[DAMAGE] = 85;
             break;
         }
     }
@@ -209,9 +229,9 @@
     //Avg damage = (100-miss)/100*dam
     //6 price; 0 Type; 1 range; 2 percent miss; 3 rds/mag; 4 damage;
     //5 fire type: {0 = bolt/pump, 1 = semi-auto., 2 = fully auto.}
-    double adjMiss = 100 - stats[2];
-    double avgDam = (adjMiss/100)*stats[4];
-    stats[6] = (stats[1] * avgDam)/2;
+    double adjMiss = 100 - stats[MISS];
+    double avgDam = (adjMiss/100)*stats[DAMAGE];
+    stats[COST] = (stats[RANGE] * avgDam)/2;
 
 
     //Print off the stats
@@ -219,37 +239,37 @@
     //5 fire type: {0 = bolt/pump, 1 = semi-auto., 2 = fully auto.}
 
     printf("Type: ");
-    switch (stats[0]) {
-        case 0:
+    switch (stats[TYPE]) {
+        case PISTOL:
             printf("Pistol");
         break;
-        case 1:
+        case SMG:
             printf("SMG");
         break;
-        case 2:
+        case SHOTGUN:
             printf("Shotgun");
         break;
-        case 3:
+        case CARBINE:
             printf("Carbine / BR");
         break;
-        case 4:
+        case AUTOMATIC:
             printf("Automatic Rifle");
         break;
-        case 5:
+        case HEAVY:
             printf("Heavy Weapon");
         break;
-        case 6:
+        case LONG_RIFLE:
             printf("Long Rifle");
         break;
     }
     printf("\n");
 
-    printf("Range: %d m\n", stats[1]);
-    printf("Percent Miss: %d%%\n",stats[2]);
-    printf("Rounds: %d\n",stats[3]);
-    printf("Damage: %d\n",stats[4]);
+    printf("Range: %d m\n", stats[RANGE]);
+    printf("Percent Miss: %d%%\n",stats[MISS]);
+    printf("Rounds: %d\n",stats[CAPACITY]);
+    printf("Damage: %d\n",stats[DAMAGE]);
     printf("Fire-type: ");
-    switch (stats[5]) {
+    switch (stats[FIRE_TYPE]) {
         case 0:
             printf("Single / Bolt");
         break;
@@ -261,5 +281,5 @@
         break;
     }
     printf("\n");
-    printf("Price: $%d\n",stats[6]);
+    printf("Price: $%d\n",stats[COST]);
  }
