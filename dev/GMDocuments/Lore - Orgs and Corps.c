f98da092e07814a491191b44078e1492809a4b83
trowl223 <t.rowland223@gmail.com>
2016-11-20 19:37:05 -0800

    Added lore on the first organisation, the Church of Eta Omega, a Lawful Good organisation that produces well-trained Paladins, but also dutiful and just Mages and Priests.

--- dev/GMDocuments/Lore - Orgs and Corps.c
+++ dev/GMDocuments/Lore - Orgs and Corps.c
@@ -8,3 +8,10 @@
 Coprorations control regions within the sectors, and are usually local to only those sectors. They are very large, but very few span multiple sectors
 
 Organsations are the entities responsible for pioneering space travel etc. and are concerned with finding artifacts. they differ from corporations in the sense that they have been araound since the onset of space travel, and are larger now then mere profit-based businesses. their goals vary from introuducing the tech they find to all people, to locking it up so no one can use it for its destrucive puroses, to simply destroying it. they are working against each other or with each other.
+
+
+
+Church of Eta Omega of the 8th sector:
+Alignment: Lawful Good
+Description: Primarily a harware research branch, Eta Omega distanced itself from the combat oriented organisations, instead promoting peaceful resolutions that see justice for all involved parties. That being said, they still believed in martial defense of your own property, and believe the threat faced by humanity is not without great concern. For this reason they maintain a standing army which frequently aid the ECSC as a mercenary branch of technology experts.
+Notes: The principal training ground of some of the best Paladins. Eta Omega instills in their trusted faithful a sense of wonderment in technology, and with this awe comes great respect. The Holy warriors from this church, including their Mages and Priests typically hold great senses of duty and justice.
